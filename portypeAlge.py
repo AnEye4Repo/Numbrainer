d1e725fb4da4832e3e2aec9abb800463861154ba
ALazyCrow <juanzinacordoba@gmail.com>
2023-11-01 13:44:53 -0400

    Different for portypeAlge, added ttk

--- portypeAlge.py
+++ portypeAlge.py
@@ -1,4 +1,5 @@
-from tkinter import *
+import tkinter as tk
+from tkinter import ttk
 
 def perform_operation():
     try:
@@ -25,39 +26,39 @@ def perform_operation():
     except ValueError:
         result_label.config(text="Error: Invalid input")
 
-window = Tk()
+window = tk.Tk()
 window.geometry("400x200")
 window.title("Basic Algebra Calculator")
 
-frame = Frame(window)
+frame = ttk.Frame(window)
 frame.pack(pady=10)
 
-label_num1 = Label(frame, text="Number 1:")
+label_num1 = ttk.Label(frame, text="Number 1:")
 label_num1.grid(row=0, column=0)
 
-entry_num1 = Entry(frame)
+entry_num1 = ttk.Entry(frame)
 entry_num1.grid(row=0, column=1)
 
-label_operator = Label(frame, text="Operator:")
+label_operator = ttk.Label(frame, text="Operator:")
 label_operator.grid(row=1, column=0)
 
-operator_var = StringVar()
+operator_var = tk.StringVar()
 operator_var.set("+")  # Default operator is addition
 
 operators = ["+", "-", "*", "/"]
-operator_menu = OptionMenu(frame, operator_var, *operators)
+operator_menu = ttk.Combobox(frame, textvariable=operator_var, values=operators)
 operator_menu.grid(row=1, column=1)
 
-label_num2 = Label(frame, text="Number 2:")
+label_num2 = ttk.Label(frame, text="Number 2:")
 label_num2.grid(row=2, column=0)
 
-entry_num2 = Entry(frame)
+entry_num2 = ttk.Entry(frame)
 entry_num2.grid(row=2, column=1)
 
-calculate_button = Button(frame, text="Calculate", command=perform_operation)
+calculate_button = ttk.Button(frame, text="Calculate", command=perform_operation)
 calculate_button.grid(row=3, columnspan=2)
 
-result_label = Label(frame, text="")
+result_label = ttk.Label(frame, text="")
 result_label.grid(row=4, columnspan=2)
 
 window.mainloop()
